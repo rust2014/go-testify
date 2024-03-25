183be3cc909a2242ec951a148b9f55610611302c
Rustam Kildeev <kildeevrustam02@gmail.com>
2024-03-25 17:18:29 +0100

    second commit

--- precode_test.go
+++ precode_test.go
@@ -18,12 +18,10 @@ func TestMainHandlerCorrectRequest(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	assert.Equal(t, http.StatusOK, responseRecorder.Code) // статус 200
+	require.Equal(t, http.StatusOK, responseRecorder.Code) // статус 200
 	responseBody := responseRecorder.Body.String()
 
-	expectedCafes := []string{"Мир кофе", "Сытый студент"}
-	list := strings.Split(responseBody, ",") // делим на элементы чтобы не счиать буквы
-	assert.ElementsMatch(t, expectedCafes, list)
+	require.NotEmpty(t, responseBody, "тело ответа не должно быть пустым") // проверка что тело ответа не пустое
 }
 
 // Неподдерживаемый город
@@ -35,13 +33,13 @@ func TestMainHandlerUnsupportedCity(t *testing.T) {
 	handler.ServeHTTP(responseRecorder, req)
 
 	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code) // 400
-	assert.Contains(t, responseRecorder.Body.String(), "неправильное значение города")
+	assert.Equal(t, responseRecorder.Body.String(), "wrong city value")
 }
 
 // Запрос с count больше доступного
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // здесь нужно создать запрос к сервису
+	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil) // здесь нужно создать запрос к сервису
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
@@ -50,8 +48,6 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	assert.Equal(t, http.StatusOK, responseRecorder.Code)
 	responseBody := responseRecorder.Body.String()
 
-	require.NotEmpty(t, responseBody)
 	list := strings.Split(responseBody, ",") // делим на элементы чтобы не счиать буквы
-	assert.Len(t, list, totalCount, "доступные кафе")
-
+	require.Len(t, list, totalCount, "доступные кафе")
 }
